1b78d351ccccc6afe7ed2eaa525a08bc6295806f
NickMuhigi <n.muhigi@alustudent.com>
2025-07-24 19:06:56 +0200

    Mapped users, trained and saved the model

--- data/images/face_recognition_model.py
+++ data/images/face_recognition_model.py
@@ -6,24 +6,31 @@ import joblib
 
 IMAGE_FEATURES_PATH = "image_features.csv"
 CUSTOMER_DATA_PATH = "../tables/merged_customer_data.csv"
-MODEL_OUTPUT_PATH = "face_recognition_model.pkl"
+MODEL_OUTPUT_PATH = "../../models/face_recognition_model.pkl"
 
 features_df = pd.read_csv(IMAGE_FEATURES_PATH)
 customer_df = pd.read_csv(CUSTOMER_DATA_PATH)
 
-user_id_map = {
+person_to_customer_id = {
     'owen': 187,
     'nicolas': 177,
     'abiodun': 189,
     'gaius': 120,
     'anissa': 103
 }
-features_df['user_id'] = features_df['person'].map(user_id_map)
 
-features_df = features_df.dropna(subset=['user_id'])
+mapping_df = pd.DataFrame(list(person_to_customer_id.items()), columns=['person', 'customer_id'])
 
-X = features_df.drop(columns=['person', 'expression', 'variation', 'user_id'])
-y = features_df['user_id'].astype(int)
+customer_df_unique = customer_df[['customer_id']].drop_duplicates()
+
+mapped_df = pd.merge(features_df, mapping_df, on='person', how='left')
+
+mapped_df = mapped_df.dropna(subset=['customer_id'])
+
+mapped_df['customer_id'] = mapped_df['customer_id'].astype(int)
+
+X = mapped_df.drop(columns=['person', 'expression', 'variation', 'customer_id'])
+y = mapped_df['customer_id']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
